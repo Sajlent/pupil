f8c3c97806461fb3f1282490531f5d46c8a3792d
lukaszfronia <lukaszfronia99@gmail.com>
2024-06-12 20:22:36 +0200

    added styles for the 404 page and add a link to the Logo

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,23 @@
+"use client";
+
+import Image from "next/image";
 import Link from "next/link";
+import Button from "@/app/ui/forms/button/button";
+import { ButtonTypes } from "@/app/types/Forms";
+import styles from "./page.module.scss";
+
+import { useAuthContext } from "@/app/providers";
 
 export default function NotFound() {
+  const { currentUser } = useAuthContext();
+
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className={styles.notFound_container}>
+      <Image src={"/images/404.png"} width={500} height={500} alt="404" />
+      <h1>Strona nie została znaleziona.</h1>
+      <Link href={currentUser ? "/noticeboard/" : "/"}>
+        <Button type={ButtonTypes.BUTTON} title="Powrót" label="Powrót" />
+      </Link>
     </div>
   );
 }
